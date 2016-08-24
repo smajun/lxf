b50faed0b79984265d133ffe940aa539131ee234
m <m>
2016-08-24 15:09:50 +0800

    v1

---
+++ do_mysql_select.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+########## prepare ##########
+
+# install mysql-connector-python:
+# pip3 install mysql-connector-python --allow-external mysql-connector-python
+
+import mysql.connector
+
+# change root password to yours:
+conn = mysql.connector.connect(user='root', password='root', database='lxf')
+
+
+
+# 运行查询:
+cursor = conn.cursor()
+cursor.execute('select * from user where id = %s', ('1',))
+values = cursor.fetchall()
+print(values)
+# 关闭Cursor和Connection:
+cursor.close()
+conn.close()
