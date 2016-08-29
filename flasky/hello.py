afd150c69a32b11931aad3c6a6506843dac7e4d6
m <m>
2016-08-29 20:21:29 +0800

    2b

--- flasky/hello.py
+++ flasky/hello.py
@@ -2,11 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return '<h1>Hellow World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
 
 if __name__ == '__main__':
     app.run(debug=True)
