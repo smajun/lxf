743e2b8459ae8e496a4cc455dad711c4c1c03669
m <m>
2016-09-02 16:08:44 +0800

    2b my test1

--- flasky/hello.py
+++ flasky/hello.py
@@ -2,18 +2,45 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask import request
+from flask import make_response
+# from flask import redirect
+from flask import abort
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hellow World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    # return '<p>Your browser is %s</p>' % user_agent
+    # return '<h1>Bad Request</h1>', 400
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
+    # return redirect('http://m.hanxinbank.com')
 
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello, %s!</h1>' % name
+
+def load_user(id):
+    if id == '1':
+        return 'duoduo'
+
+
+@app.route('/user/<id>')
+def get_user(id):
+    # def load_user(id):
+    #     if id == 1:
+    #         return 'duoduo'
+    # user = 'duoduo'
+    user = load_user(id)
+    if not user:
+        abort(404)
+        # return '<h1>Error, %s!</h1>' % user
+    return '<h1>Hello, %s!</h1>' % user
 
 
 if __name__ == '__main__':
